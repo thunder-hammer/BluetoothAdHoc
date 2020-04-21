5cf0ce20bb2687fcde1d2cc43e64b43448217209
thunder-hammer <dmsisawesome@gmail.com>
2020-04-21 03:59:13 -0600

    merge

--- bluetooth_client_linux.py
+++ bluetooth_client_linux.py
@@ -3,14 +3,15 @@ A simple Python script to send messages to a sever over Bluetooth
 using PyBluez (with Python 2).
 """
 
+from mac_addresses import *
 import bluetooth
 
-serverMACAddress = '70:C9:4E:71:88:34'
-port = 25
+serverMACAddress = mac_computer_address
+port = 0
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
 while 1:
-    text = input() # Note change to the old (Python 2) raw_input
+    text = input()
     if text == "quit":
         break
     s.send(text.encode())
