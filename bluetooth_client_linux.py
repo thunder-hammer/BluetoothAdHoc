3add820688dfa85714eb1c7820e9a749bab78cf8
Dallin Skouson <dallinskouson@Mac.app.byu.edu>
2020-04-21 03:43:57 -0600

    added receive capability?

--- bluetooth_client_linux.py
+++ bluetooth_client_linux.py
@@ -3,15 +3,33 @@ A simple Python script to send messages to a sever over Bluetooth
 using PyBluez (with Python 2).
 """
 
+from mac_addresses import *
+import threading 
 import bluetooth
 
-serverMACAddress = '70:C9:4E:71:88:34'
+serverMACAddress = linux_computer_address
 port = 25
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
-while 1:
-    text = input() # Note change to the old (Python 2) raw_input
-    if text == "quit":
-        break
-    s.send(text.encode())
+
 s.close()
+
+def sender(s):
+    while 1:
+        text = input() # Note change to the old (Python 2) raw_input
+        if text == "quit":
+            break
+        s.send(text.encode())
+
+def receiver(s):
+    size = 1024
+    client, clientInfo = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+
+snd = threading.Thread(target=receiver, args=(s,))
+rcv = threading.Thread(target=sender, args=(s,))
+rcv.start()
+snd.start()
