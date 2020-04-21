f222b6d231690d9fd9318ae531607179964c70f5
Dallin Skouson <dallinskouson@Mac.app.byu.edu>
2020-04-21 04:05:25 -0600

    added client

--- bluetooth_client_linux.py
+++ bluetooth_client_linux.py
@@ -12,8 +12,6 @@ port = 25
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
 
-s.
-
 def sender(s):
     while 1:
         text = input() # Note change to the old (Python 2) raw_input
@@ -25,11 +23,7 @@ def receiver(s):
     size = 1024
     #client, clientInfo = s.accept()
     while 1:
-<<<<<<< HEAD
-        data = s.recv(size)
-=======
         data = s.listen(size)
->>>>>>> 7c80fa7f7dd955ffaa4e3dc178cd092822399297
         if data:
             print(data)
 
